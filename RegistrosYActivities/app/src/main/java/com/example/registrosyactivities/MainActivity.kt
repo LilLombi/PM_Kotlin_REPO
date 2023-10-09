263b7b1ba3ca62c925f4515256c4b3615da7d786
LilLombi <danicharanga18@gmail.com>
2023-10-09 18:08:39 +0200

    Lel

--- RegistrosYActivities/app/src/main/java/com/example/registrosyactivities/MainActivity.kt
+++ RegistrosYActivities/app/src/main/java/com/example/registrosyactivities/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.registrosyactivities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.registrosyactivities.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //setContentView(R.layout.activity_main)
+        binding.boton.setOnClickListener{
+            inVentana2()
+        }
+    }
+
+    private fun inVentana2() {
+        var miIntent: Intent = Intent(this, Ventana2::class.java)
+        startActivity(miIntent)
     }
 }
